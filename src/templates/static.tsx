f3814e9e2d27dac8a40b79107aa0667232e62436
lambdaFun <61638671+lambdaFun94@users.noreply.github.com>
2022-07-25 16:42:00 -0400

    Initial commit

---
+++ src/templates/static.tsx
@@ -0,0 +1,80 @@
+/**
+ * This is an example of how to create a static template that uses getStaticProps to retrieve data.
+ */
+
+import * as React from "react";
+import { useState } from "react";
+import fetch from "fetch-everywhere";
+import { Pokemon } from "pokenode-ts";
+import {
+  Template,
+  GetPath,
+  TransformProps,
+  TemplateConfig,
+  TemplateProps,
+  TemplateRenderProps,
+} from "@yext/pages";
+
+/**
+ * Not required depending on your use case.
+ */
+export const config: TemplateConfig = {
+  // The name of the feature. If not set the name of this file will be used (without extension).
+  // Use this when you need to override the feature name.
+  name: "pokemon",
+};
+
+/**
+ * A local type for transformProps. This could live in src/types but it's generally
+ * best practice to keep unshared types local to their usage.
+ */
+ type PokemonData = TemplateProps & { pokemon: Pokemon };
+
+ /**
+  * Used to either alter or augement the props passed into the template at render time.
+  * This function will be run during generation and pass in directly as props to the default
+  * exported function.
+  *
+  * This can be used when data needs to be retrieved from an external (non-Knowledge Graph)
+  * source. This example calls a public API and returns the data.
+  *
+  * If the page is truly static this function is not necessary.
+  */
+ export const transformProps: TransformProps<PokemonData> = async (data) => {
+   const url = import.meta.env.YEXT_PUBLIC_POKEMON_API_BASE_URL;
+   const pokemon = (await fetch(url).then((res: any) => res.json())) as Pokemon;
+
+   return { ...data, pokemon };
+ };
+
+/**
+ * Defines the path that the generated file will live at for production.
+ *
+ * NOTE: This currently has no impact on the local dev path. Local dev urls currently
+ * take on the form: featureName/entityId
+ */
+export const getPath: GetPath<PokemonData> = () => {
+  return `static/${Math.random().toString()}`;
+};
+
+type PokemonRenderData = TemplateRenderProps & { pokemon: Pokemon };
+
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct result from `getStaticProps`.
+ */
+const Static: Template<PokemonRenderData> = ({relativePrefixToRoot, path, document, pokemon}) => {
+  const { name } = pokemon;
+
+  const [num, setNum] = useState<number>(0);
+
+  return (
+    <>
+      <div>Hello from {name}</div>
+      <button onClick={() => setNum(num + 1)}>Click me</button>
+      Num: {num}
+    </>
+  );
+};
+
+export default Static;
